59bb58d5cd87d4169df23b52d095cd921b68e970
Paulo Cesar Loayza <pcloayza@gmail.com>
2020-04-07 20:49:12 -0400

    insert product completed

---
+++ fnc_cosmosdb/fnc_cosmosdb/Helpers/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace fnc_cosmosdb.Helpers
+{
+    public static class Constants
+    {
+        public const string COSMOS_DB_CONNECTION_STRING = "AccountEndpoint=https://bdubicua.documents.azure.com:443/;AccountKey=0oerY4CLkrQf8qUrWozuYK4mS9DNMM8nHBHiEe33AKWTmFjqz5eascd8e19aMxZRWY2YZs0AEXjqgcqM8aZNyQ==;";                                                          
+        public const string COSMOS_DB_DATABASE_NAME = "bdfnc";
+        public const string COSMOS_DB_CONTAINER_NAME = "product";
+        public const string COSMOS_DB_ACCOUNT_NAME = "bdubicua";
+    }
+}
--- fnc_cosmosdb/fnc_cosmosdb/ProductInsert.cs
+++ fnc_cosmosdb/fnc_cosmosdb/ProductInsert.cs
@@ -10,29 +10,43 @@ namespace fnc_cosmosdb
     using Microsoft.Extensions.Logging;
     using Newtonsoft.Json;
     using Models;
+    using Helpers;
 
     public class ProductInsert
     {
         [FunctionName(nameof(ProductInsert))]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                        databaseName: Constants.COSMOS_DB_DATABASE_NAME,
+                        collectionName:Constants.COSMOS_DB_CONTAINER_NAME,
+                        ConnectionStringSetting = "StrCosmos"
+                        )] IAsyncCollector<object> products,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var data = JsonConvert.DeserializeObject<Product>(requestBody);
-            var product = new Product
+            IActionResult returnValue = null;
+            try
             {
-                ProductId = data.ProductId,
-                Provider=data.Provider,
-                Name=data.Name,
-                Price=data.Price
-            };
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                var data = JsonConvert.DeserializeObject<Product>(requestBody);
+                var product = new Product
+                {
+                    ProductId = data.ProductId,
+                    Provider = data.Provider,
+                    Name = data.Name,
+                    Price = data.Price
+                };
 
-            string responseMessage = product.Name;
-
-            return new OkObjectResult(responseMessage);
+                await products.AddAsync(product);
+                log.LogInformation($"Product Inserted {product.Name}");                
+                returnValue = new OkObjectResult(product);
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Could not insert product. Exception: {ex.Message}");
+                returnValue = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            return returnValue;
         }
     }
 }
